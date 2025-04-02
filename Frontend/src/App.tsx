beb78b5eea7156adcdafc15713c0572bd365e77b
ginnysingh789 <ginnysangral786@gmail.com>
2025-04-02 23:25:17 +0530

    Second Frontent Commit

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -9,12 +9,16 @@ function App() {
 
 
   return (
-    <div className='m-2 '>
+    <div className='p-4'>
+      <div className='flex justify-end gap-2'>
+        <Button variant='primary' text='Add Content' size='md' starticon={<PlusIcon size='md' />} ></Button>
+        <Button variant="secondary" text='Share Brain' size='md' starticon={<ShareIcon size='md' />}></Button>
+      </div>
+      <div className='flex gap-2'>
+        <Card title='Project Idea' type='Youtube' link='https://www.youtube.com/watch?v=89CurjyigWA' />
+        <Card title='Project Idea' type='twitter' link='https://x.com/elonmusk/status/1907323764729094230' />
+      </div>
 
-      <Button variant='primary' text='Add Content' size='md' starticon={<PlusIcon size='md' />} ></Button>
-      <Button variant="secondary" text='Share Brain' size='md' starticon={<ShareIcon size='md' />}></Button>
-      <Card/>
-     
 
     </div>
   )
--- Frontend/src/Components/Card.tsx
+++ Frontend/src/Components/Card.tsx
@@ -1,10 +1,10 @@
 import { ShareIcon } from "../Icons/ShareIcon"
-interface CardType{
-    title:string,
-    link:string,
-    type:"twitter"|"Youtube"
+interface CardType {
+    title: string,
+    link: string,
+    type: "twitter" | "Youtube"
 }
-export const Card = (prop:CardType) => {
+export const Card = (prop: CardType) => {
     return (
         <div>
             <div className="p-8 bg-white rounded-md max-w-72 shadow-md border  border-slate-100">
@@ -13,10 +13,11 @@ export const Card = (prop:CardType) => {
                         <div className="flex items-center pr-2">
                             <ShareIcon size="md" />
                         </div>
-                        ProjectIdeas
+                        {prop.title}
                     </div>
                     <div className="flex">
                         <div className="pr-2 text-gray-700"> <ShareIcon size="md" />
+                            <a href={prop.link} target="_black"> </a>
                         </div>
                         <div className=" text-gray-700"> <ShareIcon size="md" />
                         </div>
@@ -24,23 +25,22 @@ export const Card = (prop:CardType) => {
                 </div>
                 {/* Embed Youtube video */}
                 <div className="w-full aspect-video mt-4">
-          {/* <iframe
-            className="w-full h-full rounded-md"
-            src="https://www.youtube.com/embed/4XVvbZj794o?si=PM-5Cl39IHJ0vppg"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerPolicy="strict-origin-when-cross-origin"
-            allowFullScreen
-          ></iframe> */}
-       
-                    <blockquote className="twitter-tweet pr-3">
-            <a href="https://twitter.com/username/status/807811447862468608"></a> 
-            </blockquote>
-            
-        </div>
+                    {prop.type === "Youtube" &&
+                        <iframe
+                        className="w-full h-full rounded-md"
+                        src={prop.link.replace("watch?v=", "embed/")}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                      ></iframe>
+                    }
+                    {prop.type === "twitter" && <blockquote className="twitter-tweet ">
+                        <a href={prop.link.replace('x.com','twitter.com')}></a>
+                    </blockquote>}
+                </div>
             </div>
-
         </div>
     )
 }
